397d171e4311d55379702babd2675b21099fb5c0
Fábio Wilian <cursosfb.01@gmail.com>
2022-07-03 14:41:37 -0300

    Criado e estilizado a pagina de login

---
+++ sistema/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>TesteS</title>
+</head>
+<body>
+    <main class="main">
+        <form action="" method="">
+            <div class="">
+                <div class="style-input">
+                    <input class="input" type="text" placeholder="USERNAME" name="email">
+                    <p class="icon-email"></p>
+                </div>
+                <div class="style-input">
+                    <input class="input" type="password" placeholder="**********" name="senha">
+                    <div class="icon-bg">
+                        <p class="icon-senha"></p>
+                    </div>
+
+                </div>
+                <input class="btn" type="submit" value="LOGIN" s>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
