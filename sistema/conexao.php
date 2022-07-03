2f5e9f8525a15e947a59a7050d452691ba1e1030
Fábio Wilian <cursosfb.01@gmail.com>
2022-07-03 14:52:18 -0300

    Relizado a conexão com o Banco de Dados

---
+++ sistema/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Dados do servidor
+$servidor = '127.0.0.1:3307';
+$banco = 'melflash_sql';
+$usuario = 'root';
+$senha = '';
+
+// Conexão com o banco
+
+date_default_timezone_set('America/Sao_Paulo');
+
+try {
+    $pdo = new PDO("mysql:host=$servidor;dbname=$banco", "$usuario", "$senha");
+} catch (PDOException $e){
+    echo 'Erro ao se conectar ao Banco de Dados' .$e;
+}
+
+echo ('teste')
+
+
+?>
