a9f607e80a3aaa4c194df4102afe3928d30f2490
Fábio Wilian <cursosfb.01@gmail.com>
2022-07-04 18:37:43 -0300

    Adicionado uma sessão para quando o usuario logar e deslogar

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,4 +1,5 @@
 <?php
+    @session_start();
     require_once("conexao.php");
 
 
@@ -7,8 +8,15 @@
 
     $query = $pdo -> query("SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'");
     $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
-    $total_reg = count($resultado);
+    $total_reg = @count($resultado);
     if($total_reg > 0) {
+
+        $_SESSION['id'] = $res[0]['id'];
+        $_SESSION['nivel'] = $res[0]['nivel'];
+        $_SESSION['nome'] = $res[0]['nome'];
+
+
+
         echo "<script>window.location='painel'</script>";
     } else {
         echo "<script>window.alert('Usuario ou Senha Incorretos!')</script>";
---
+++ sistema/painel/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// echo "<script>window.location='../index.php'</script>";
+
+
+
+
+?>
+
+<a href="logout.php">sair</a>
